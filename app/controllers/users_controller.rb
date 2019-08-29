ded124402e37260508283698849b7438bd046620
shiogoma <azm0820@gmail.com>
2019-08-29 16:49:05 +0900

    crreate-userview

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
   def mypage
   end
+
+  def 
+    signup
+  end
+
+  def
+    identification
+  end
+
+  def
+    cardselect    
+  end
+
+  def
+    logout
+  end
+
+  def
+    login
+  end
 end
