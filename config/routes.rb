db624afef315a87dbb412ef3b91a573e4b787861
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-29 19:34:21 +0900

    fix
    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   root 'items#index'
   get '/mypage' => 'users#mypage'
-=======
-  root 'items#index'
->>>>>>> dac12bb4a97271d15f4df9780c97fdc3d266b49a
 end
