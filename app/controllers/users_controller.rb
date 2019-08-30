3b22a3e000ccb868ec222a0afb40d1a933805448
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-30 10:48:58 +0900

    rename haml file
    hamlファイル名を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,46 @@
 class UsersController < ApplicationController
 # マイページアクションはマイページのファイルを出すためのダミーです
-def mypage
-end
+  def mypage
+  end
 
-# レジストレーションはレジストレーションファイル確認のためのダミー
-def resistration
-end
-# ダミーです
-def tell
-end
+  # レジストレーションはレジストレーションファイル確認のためのダミー
+  def resistration
+  end
   # ダミーです
-def address
-end
-  # ダミーです
-def credit
-end
-  # ダミーです
-def finish
-end
+  def tell
+  end
+    # ダミーです
+  def address
+  end
+    # ダミーです
+  def credit
+  end
+    # ダミーです
+  def finish
+  end
 
-def profile_editer
-end
+  def profile_editer
+  end
 
 
 
-def 
-  signup
-end
+  def 
+    signup
+  end
 
-def
-  identification
-end
+  def
+    identification
+  end
 
-def
-  cardselect    
-end
+  def
+    cardselect    
+  end
 
-def
-  logout
-end
+  def
+    logout
+  end
 
-def
-  login
-end
+  def
+    login
+  end
 end
