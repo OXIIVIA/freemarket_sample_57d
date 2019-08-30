77a6ef67c812c2bf4603e55db8f4056593ccf3bf
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-30 10:44:09 +0900

    Update routes.rb
    ルーティングを仮置き

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :users, only: [:index]
+  # 以下のルーティングはビューページ作成までの仮置き。
+  # LGTMがでたら、referencesに書き換える
+  get '/mypage' => 'users#mypage'
+  get '/sign_up' => 'users#resistration'
+  get '/sign_up/2' => 'users#tell'
+  get '/sign_up/3' => 'users#address'
+  get '/sign_up/4' => 'users#credit'
+  get '/sign_up/5' => 'users#finish'
+  get '/mypage/2' => 'users#edit'
+
+  get '/signup' => 'users#signup'
+  get '/identification' => 'users#identification'
+  get '/cardselect' => 'users#cardselect'
+  get '/logout' => 'users#logout'
+  get '/login' => 'users#login'
 end
