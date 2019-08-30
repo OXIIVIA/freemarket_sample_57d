dbcb860c89dd3573a680c9ba6442bede550467d1
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-30 10:46:17 +0900

    Update users_controller.rb
    アクションの書き換え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,46 @@
 class UsersController < ApplicationController
+# マイページアクションはマイページのファイルを出すためのダミーです
+def mypage
+end
 
-  def index
-  end
+# レジストレーションはレジストレーションファイル確認のためのダミー
+def resistration
+end
+# ダミーです
+def tell
+end
+  # ダミーです
+def address
+end
+  # ダミーです
+def credit
+end
+  # ダミーです
+def finish
+end
 
+def profile_editer
+end
+
+
+
+def 
+  signup
+end
+
+def
+  identification
+end
+
+def
+  cardselect    
+end
+
+def
+  logout
+end
+
+def
+  login
+end
 end
