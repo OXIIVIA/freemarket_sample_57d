bbd34a9892d7a79a048981cfb2de449bd052c35a
Toru Oshima <selfmadetool0213@icloud.com>
2019-09-02 14:10:08 +0900

    Add associations
    アソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
+  has_many :sns_credentials, dependent: :destroy
+  
   def self.find_for_oauth(auth)
     sns = SnsCredential.where(uid: auth.uid, provider: auth.provider).first
     unless sns
