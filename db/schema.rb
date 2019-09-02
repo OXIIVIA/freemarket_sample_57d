8003e4afa2502f434306692bc1e2efda12baff98
Toru Oshima <selfmadetool0213@icloud.com>
2019-09-02 11:25:51 +0900

    Add Culumns to Users
    Usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_060755) do
+ActiveRecord::Schema.define(version: 2019_09_02_022423) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_08_31_060755) do
     t.string "nickname", null: false
     t.string "birthdate", null: false
     t.text "profile"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
