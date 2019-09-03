135b9f4521a3531f9991c185922a49ccf136aa77
Toru Oshima <selfmadetool0213@icloud.com>
2019-09-03 12:42:46 +0900

    Fix errors
    エラーを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_045008) do
+ActiveRecord::Schema.define(version: 2019_09_02_050449) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 2019_09_02_045008) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "uid"
+    t.string "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,6 +67,8 @@ ActiveRecord::Schema.define(version: 2019_09_02_045008) do
     t.string "image", null: false
     t.string "nickname", null: false
     t.text "profile"
+    t.string "provider"
+    t.string "uid"
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
