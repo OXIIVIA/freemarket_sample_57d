cc16b49aee7ea9577a839ed4f65d1689dd641a5f
yufauefa <yuto.program@gmail.com>
2019-09-05 17:45:12 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_035517) do
+ActiveRecord::Schema.define(version: 2019_09_05_082626) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2019_09_05_035517) do
     t.integer "brand_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.text "image"
   end
 
   create_table "personals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
