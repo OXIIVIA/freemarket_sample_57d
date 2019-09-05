409abf3e1522059de53b54407ecd66f751bb1440
Toru Oshima <selfmadetool0213@icloud.com>
2019-09-05 12:29:01 +0900

    Update schema.rb
    rake db:migrateの実行

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 2019_09_04_101044) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "uid"
+    t.string "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,6 +65,8 @@ ActiveRecord::Schema.define(version: 2019_09_04_101044) do
     t.datetime "updated_at", null: false
     t.string "nickname", null: false
     t.text "profile"
+    t.string "provider"
+    t.string "uid"
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
