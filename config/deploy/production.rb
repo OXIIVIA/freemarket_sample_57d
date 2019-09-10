b73f7d687d812103c61e9e9232900cf94da6eaf7
Toru Oshima <selfmadetool0213@icloud.com>
2019-09-10 15:17:37 +0900

    Update production.rb
    Elastic IPを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.114.22.119', user: 'ec2-user', roles: %w{app db web}
+server '52.197.95.209', user: 'ec2-user', roles: %w{app db web}
 
 # set :rails_env, "production"
 # set :unicorn_rack_env, "production"
