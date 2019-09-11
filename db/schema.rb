414161b9e0385ffb2c81a53473506b9877db7343
shiogoma <azm0820@gmail.com>
2019-09-11 15:31:12 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_092551) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
     t.datetime "created_at", null: false
@@ -120,5 +121,4 @@ ActiveRecord::Schema.define(version: 2019_09_10_092551) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "cards", "users"
 end
