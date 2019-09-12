5b9967ce2a93a4d86ed5bcde6560c60ea18c7613
Toru Oshima <selfmadetool0213@icloud.com>
2019-09-12 17:10:02 +0900

    Update items_controller.rb
    authenticate_user!に例外を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   before_action :set_product, only: [:show, :edit, :update]
   before_action :set_seler, only: :show 
 
