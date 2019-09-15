dd4c0cfcb7745c7c3853fd1d6844601b7f51f091
shiogoma <azm0820@gmail.com>
2019-09-15 20:04:25 +0900

    商品バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,17 @@ class Item < ApplicationRecord
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
 
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :condition, presence: true
+  validates :deliver_date, presence: true
+  validates :postage, presence: true
+  validates :category_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :price,numericality: { only_integer: true,greater_than: 299, less_than: 10000000}
+  validates :image, presence: true
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   
