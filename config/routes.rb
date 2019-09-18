f599e9a7687c399b792d60d68661faeab83f9957
Toru Oshima <selfmadetool0213@icloud.com>
2019-09-18 18:17:01 +0900

    Update routes.rb
    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,23 +6,25 @@ Rails.application.routes.draw do
       get 'purchase/:id', to: 'items#purchase', as: 'purchase'
       post 'pay/:id', to: 'items#pay', as: 'pay'
       get '/search', to: 'items#search', as: 'search'
-      get '/mypage/:id', to: 'users#mypage', as: "mypage"
-      get '/sign_up' => 'users#resistration'
-      get '/sign_up/2' => 'users#tell'
-      get '/sign_up/3' => 'users#address'
-      get '/sign_up/4' => 'users#credit'
-      get '/sign_up/5' => 'users#finish'
-      get '/mypage/2' => 'users#edit'
-      get '/signup' => 'users#signup'
-      get '/identification' => 'users#identification'
-      get '/cardselect' => 'users#cardselect'
-      get '/logout', to: 'users#logout', as: "logout"
-      get '/login', to: 'users#login', as: "login"
-      get '/sell' => 'items#sell' 
-      get 'sell/get_category_children' => 'items#get_category_children', defaults: { format: 'json' }
-      get 'sell/get_category_grandchildren' => 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
   end
+
+  get '/mypage/:id', to: 'users#mypage', as: "mypage"
+  get '/sign_up' => 'users#resistration'
+  get '/sign_up/2' => 'users#tell'
+  get '/sign_up/3' => 'users#address'
+  get '/sign_up/4' => 'users#credit'
+  get '/sign_up/5' => 'users#finish'
+  get '/mypage/2' => 'users#edit'
+  get '/signup' => 'users#signup'
+  get '/identification' => 'users#identification'
+  get '/cardselect' => 'users#cardselect'
+  get '/logout', to: 'users#logout', as: "logout"
+  get '/login', to: 'users#login', as: "login"
+  get '/sell' => 'items#sell' 
+  get 'sell/get_category_children' => 'items#get_category_children', defaults: { format: 'json' }
+  get 'sell/get_category_grandchildren' => 'items#get_category_grandchildren', defaults: { format: 'json' }
+
   resources :signup do
     collection do
       get 'step1'
